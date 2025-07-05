b4e76061a9381bc919b901eee441b285b207adb7
Ravi Ojha <ivarojha@gmail.com>
2025-07-06 02:29:52 +0530

    Overlap chat window on bubble

--- src/components/chat-bubble.js
+++ src/components/chat-bubble.js
@@ -43,12 +43,12 @@ export class ChatBubble {
 
   createChatIcon() {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('viewBox', '0 0 24 24');
-    svg.setAttribute('width', '24');
-    svg.setAttribute('height', '24');
+    svg.setAttribute('viewBox', '0 0 256 256');
+    svg.setAttribute('width', '32');
+    svg.setAttribute('height', '32');
 
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', 'M20 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h4l4 4 4-4h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z');
+    path.setAttribute('d', 'M216,48H40A16,16,0,0,0,24,64V224a15.84,15.84,0,0,0,9.25,14.5A16.05,16.05,0,0,0,40,240a15.89,15.89,0,0,0,10.25-3.78l.09-.07L83,208H216a16,16,0,0,0,16-16V64A16,16,0,0,0,216,48ZM160,152H96a8,8,0,0,1,0-16h64a8,8,0,0,1,0,16Zm0-32H96a8,8,0,0,1,0-16h64a8,8,0,0,1,0,16Z');
 
     svg.appendChild(path);
     return svg;
